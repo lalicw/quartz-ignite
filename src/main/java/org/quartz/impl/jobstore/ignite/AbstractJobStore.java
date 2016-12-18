cb6cb70dc1efabc9fea28fdadedf7977042c935c
lalicw <mhsjlove@gmail.com>
2016-12-18 22:48:36 +0800

    create abstract job store

---
+++ src/main/java/org/quartz/impl/jobstore/ignite/AbstractJobStore.java
@@ -0,0 +1,11 @@
+package org.quartz.impl.jobstore.ignite;
+
+import org.quartz.spi.JobStore;
+
+/**
+ * 
+ * @since Dec 18, 2016 10:44:33 PM
+ * @author libra
+ */
+public abstract class AbstractJobStore implements JobStore {
+}
